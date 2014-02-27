0baf8c65e7eda20d10df4caf2e32c9a09080606f
Ben Zhou <Ben@dn0a22827d.sunet>
2014-02-27 00:01:54 -0800

    implemented question, still need to implement quiz

---
+++ src/quiz/Question.java
@@ -0,0 +1,37 @@
+package quiz;
+
+import java.util.ArrayList;
+
+public class Question {
+
+	public ArrayList<String> answers; //there is potentially more than 1 answer per question
+	public int score;
+	public static int DEFAULT_SCORE = 5;
+	
+	public Question() {
+		this.score = DEFAULT_SCORE;
+		answers = new ArrayList<String>();
+	}
+	
+	public Question(int score) {
+		this.score = score;
+		answers = new ArrayList<String>();
+	}
+	
+	public void addAnswer(String answer) {
+		answers.add(answer);
+	}
+	
+	public ArrayList<String> getAnswers() {
+		return answers;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void resetScore(int newScore) {
+		this.score = newScore;
+	}
+	
+}
