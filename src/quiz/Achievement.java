6bc1899d29df42795a0dbcbe0909b861499b4337
mikalway <mikalway@stanford.edu>
2014-03-01 21:20:55 -0800

    Added achievement

--- src/quiz/Achievement.java
+++ src/quiz/Achievement.java
@@ -5,5 +5,21 @@ public class Achievement {
 	private String name;
 	private String description;
 	
+	public Achievement(String image, String name, String description){
+		this.image = image;
+		this.name = name;
+		this.description = description;
+	}
 	
+	public String getImage(){
+		return this.image;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getDescription(){
+		return this.description;
+	}
 }
