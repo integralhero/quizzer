5fb610ea9d0f2c240a19b950510d19e644936593
mikalway <mikalway@stanford.edu>
2014-03-09 00:01:32 -0800

    AchievementDao changed

--- src/quiz/AchievementDao.java
+++ src/quiz/AchievementDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class AchievementDao {
 	
@@ -43,7 +44,55 @@ public class AchievementDao {
 			e.printStackTrace();
 		}
 	}
+
+	public static ArrayList<Achievement> getAchievements(int userID){
+		ArrayList<Achievement> temp = new ArrayList<Achievement>();
+		
+		try {
+
+			String command = "SELECT * FROM achievement_user_index WHERE userID = " + userID;
+			
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery(command);
+			
+			while(rs.next()){
+				String command2 = "SELECT * FROM achievements WHERE ID = " + rs.getInt("achievementID");
+			
+				Statement statement2 = connection.createStatement();
+				ResultSet rs2 = statement2.executeQuery(command2);
+				while(rs2.next()){
+					temp.add(getAchievementByID(rs2.getInt("ID")));
+				}
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return temp;
+	}
 	
+	private static Achievement getAchievementByID(int ID){
+		Achievement temp = new Achievement("","","");
+		try {
+
+			String command = "SELECT * FROM achievement WHERE ID = " + ID;
+			
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery(command);
+			if(rs.next()){
+				String image = rs.getString("image");
+				String name = rs.getString("name");
+				String description = rs.getString("description");
+				temp = new Achievement(image, name, description);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return temp;
+	}
 	
 	
 }
