083200bc9b766338d790a6699671c23bb761bdb4
David Jiang <dnjiang@stanford.edu>
2014-03-09 20:48:06 -0700

    Made quiz landing page

--- WebContent/js/multiple_pages.js
+++ WebContent/js/multiple_pages.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	$('div.question:gt(0)').hide();
+	$('div.question:lt(1) > ul li.previous').hide();
+	$('div.question:last-child > ul li.next').hide();
+	
 	$(document).on('click', '.next', function() {
 		$(this).parent().closest("div.question").hide();
 		$(this).parent().closest("div.question").next().show();
@@ -7,5 +10,24 @@ $(document).ready(function(){
 	$(document).on('click', '.previous', function() {
 		$(this).parent().closest("div.question").hide();
 		$(this).parent().closest("div.question").prev().show();
+	});
+	$(document).on('keyup', 'input.answerField', function(){
+		
+		var sibs = $(this).siblings('.hiddenAnswer');
+		var userResp = $(this).val();
+		var isCorrect = false;
+		$(sibs).each(function() {
+			if($(this).val() == userResp) {
+				isCorrect = true;
+				
+			}			
+		});
+		if(isCorrect) {
+			$(this).siblings('.feedback').html("Correct!");
+		}
+		else {
+			$(this).siblings('.feedback').html("Wrong!");
+		}
+		
 	});
 });
